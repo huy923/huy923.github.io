e024199f082da515b5b1f268bf979a42c0109878
huy923 <huyxam233@gmail.com>
2023-12-28 10:08:49 +0700

    more aos

---
+++ Bootstrap/js/main.js
@@ -0,0 +1,53 @@
+let filterarray = [];
+
+let galleryarray = [
+    {
+        id: 1,
+        name: "Iron Flame",
+        src: "./img/book.jpg",
+        desc: "Rebecca Yarros"
+    },
+    {
+        id: 2,
+        name: "Allegiant",
+        src: "./img/Allegiant.jpg",
+        desc: "Rebecca Yarros"
+    },
+    {
+        id: 3,
+        name: "Come Hungry",
+        src: "./img/Come Hungryjpg.jpg",
+        desc: "Rebecca Yarros"
+    },
+    {
+        id: 4,
+        name: "Dracula",
+        src: "./img/Draculajpg.jpg",
+        desc: "Rebecca Yarros"
+    },
+    {
+        id: 5,
+        name: "Edith Holler",
+        src: "./img/Edith Holler.jpg",
+        desc: "Rebecca Yarros"
+    },
+    {
+        id: 6,
+        name: "Elon Musk",
+        src: "./img/Elon Musk.jpg",
+        desc: "Rebecca Yarros"
+    },
+    {
+        id: 7,
+        name: "Fox in Socks",
+        src: "./img/Fox in Socks.jpg",
+        desc: "Rebecca Yarros"
+    }
+];
+function showgallery(array) {
+    for (var i = 0; i < array.length; i++) {
+        document.getElementById("cardd").innerHTML += `
+        <div`;
+        
+    }
+}
